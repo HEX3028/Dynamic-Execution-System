0be2667506a2516e18b1f0dca8c123d728bbbe51
HEX3028 <t7060832@gmail.com>
2026-05-14 12:02:21 -0500

    Create Demonstration.lua

---
+++ Demonstration.lua
@@ -0,0 +1,6 @@
+--[[ How to require with executor.
+"require(module id)" This doesnt work.
+Instead you have to require ingame modules.
+like require(game.Workspace.Module)
+To use those types of scripts use the "Scripts" tab.
+]]
